0fd3f6ab9e18dfa1751b05696c39406e012c6a1d
BuildTools <unconfigured@null.spigotmc.org>
2016-02-17 16:25:49 -0500

    Initial commit

---
+++ src/team6/Core.java
@@ -0,0 +1,17 @@
+/**
+ * The core class for this project.
+ * @author Scott Pomerville
+ * @author Monica Fraser
+ * @author Katlyn Kiekhaefer
+ * @author Nicholas Mell
+ */
+package team6;
+
+public class Core {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
