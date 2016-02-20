32ada89f7fe78057f9fc6960f575cc55a7aba5c0
BuildTools <unconfigured@null.spigotmc.org>
2016-02-19 22:04:20 -0500

    Base day progression
    Added some getters and setters for Event and Tribute (Not all of them)
    necessary. Also added functionality for the day to progress.

--- src/Tribute.java
+++ src/Tribute.java
@@ -7,6 +7,23 @@ public class Tribute {
 	private int health;
 	private Item[] items;
 	private int strength;
+	private String event;
 	
 	//TODO add getters and setters
+	int getStrength(){
+		return strength;
+	}
+	
+	String getName(){
+		return name;
+	}
+	
+	void setEvent(Event event, Tribute killer){
+		if(killer == null){
+			this.event = event.getString(this);
+		}
+		else{
+			this.event = event.getString(this, killer);
+		}
+	}
 }
