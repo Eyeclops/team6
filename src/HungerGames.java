bfc87bad1debe66519f8f85298f69b573b7272cb
kskiekha <kskiekha@mtu.edu>
2016-04-06 13:09:20 -0400

    Update HungerGames.java

--- src/HungerGames.java
+++ src/HungerGames.java
@@ -26,7 +26,7 @@ public class HungerGames extends JFrame implements ActionListener{
 	JButton autofill;
 	JButton userSettings;
 	JPanel setUpPanel;
-
+	
 	public HungerGames(String title){
 		super(title);
 	}
@@ -38,14 +38,14 @@ public class HungerGames extends JFrame implements ActionListener{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container pane = getContentPane();
 		pane.setLayout(new FlowLayout());
-
+		
 		cardPanel = new JPanel(new CardLayout());
-
-
+		
+		
 		setUpPanel = new JPanel();
 		setUpPanel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-
+		
 		//top message
 		JLabel topMessage = new JLabel();
 		topMessage.setText("<html><center>Welcome to the Hunger Games!<br><br>"
@@ -60,9 +60,9 @@ public class HungerGames extends JFrame implements ActionListener{
 		c.ipadx = 20;
 		c.ipady = 20;
 		c.gridwidth = 4;
-
+		
 		setUpPanel.add(topMessage, c);
-
+		
 		//male header
 		JLabel male = new JLabel("Male");
 		c.gridx = 1;
@@ -70,9 +70,9 @@ public class HungerGames extends JFrame implements ActionListener{
 		c.ipadx = 0;
 		c.ipady = 0;
 		c.gridwidth = 1;
-
+		
 		setUpPanel.add(male, c);
-
+		
 		//female header
 		JLabel female = new JLabel("Female");
 		c.gridx = 2;
@@ -80,13 +80,13 @@ public class HungerGames extends JFrame implements ActionListener{
 		c.ipadx = 0;
 		c.ipady = 0;
 		c.gridwidth = 1;
-
+		
 		setUpPanel.add(female, c);
-
+		
 		districts = new JLabel[12];
 		maleTributes = new JTextField[12];
 		femaleTributes = new JTextField[12];
-
+		
 		for(int i = 0; i < 12; i++) {
 			districts[i] = new JLabel("District " + (i + 1));
 			c.gridx = 0;
@@ -95,7 +95,7 @@ public class HungerGames extends JFrame implements ActionListener{
 			c.ipady = 10;
 			c.gridwidth = 1;
 			setUpPanel.add(districts[i], c);
-
+			
 			maleTributes[i] = new JTextField(20);
 			c.gridx = 1;
 			c.gridy = 2 + i;
@@ -104,7 +104,7 @@ public class HungerGames extends JFrame implements ActionListener{
 			c.gridwidth = 1;
 			c.insets = new Insets(4,4,4,4);
 			setUpPanel.add(maleTributes[i], c);
-
+			
 			femaleTributes[i] = new JTextField(20);
 			c.gridx = 2;
 			c.gridy = 2 + i;
@@ -114,7 +114,7 @@ public class HungerGames extends JFrame implements ActionListener{
 			c.insets = new Insets(4,4,4,4);
 			setUpPanel.add(femaleTributes[i], c);
 		}
-
+		
 		submit = new JButton("Let the Games Begin!");
 		c.gridx = 0;
 		c.gridy = 14;
@@ -124,7 +124,7 @@ public class HungerGames extends JFrame implements ActionListener{
 		c.insets = new Insets(10,10,10,10);
 		setUpPanel.add(submit, c);
 		submit.addActionListener(this);
-
+		
 		JPanel filler = new JPanel();
 		filler.setBorder(new EmptyBorder(5, 5, 5, 5));
 		c.gridy = 15;
@@ -132,7 +132,7 @@ public class HungerGames extends JFrame implements ActionListener{
 		c.gridwidth = 4;
 		c.ipady = 20;
 		setUpPanel.add(filler, c);
-
+		
 		userSettings = new JButton("Settings...");
 		c.insets = new Insets(0,0,0,0);
 		c.anchor = GridBagConstraints.SOUTHWEST;
@@ -143,7 +143,7 @@ public class HungerGames extends JFrame implements ActionListener{
 		c.gridwidth = 2;
 		setUpPanel.add(userSettings, c);
 		userSettings.addActionListener(this);
-
+		
 		autofill = new JButton("I don't have time for this");
 		c.anchor = GridBagConstraints.SOUTHEAST;
 		c.gridx = 2;
@@ -151,13 +151,13 @@ public class HungerGames extends JFrame implements ActionListener{
 		c.gridwidth = 2;
 		setUpPanel.add(autofill, c);
 		autofill.addActionListener(this);
-
+		
 		pane.add(cardPanel);
 		cardPanel.add(setUpPanel, "Setup");
 		CardLayout l = (CardLayout) cardPanel.getLayout();
 		l.show(cardPanel, "Setup");
 	}
-
+	
 	public static void main(String[] args) {
 		HungerGames app = new HungerGames("Hunger Games Simulator");
 		app.init();
@@ -166,53 +166,10 @@ public class HungerGames extends JFrame implements ActionListener{
 		app.pack();
 		app.setVisible(true);
 
-		Item spear = new Item("spear", 20);
-		Item sword = new Item("sword", 20);
-		Item pocketKnife = new Item("pocket knife", 5);
-		Item switchblade = new Item("switchblade", 5);
-		Item bowieKnife = new Item("bowie knife", 10);
-		Item pistol = new Item("pistol", 25);
-		Item rifle = new Item("rifle", 30);
-		Item wrench = new Item("wrench", 5);
-		Item pipe = new Item("pipe", 5);
-		Item club = new Item("club", 5);
-		Item bow = new Item("bow", 20);
-		Item crossbow = new Item("crossbow", 25);
-		Item hatchet = new Item("hatchet", 10);
-		Item axe = new Item("axe", 20);
-
-		ArrayList<Event> events = new ArrayList<Event>();
-		events.add(new Event("_v1 killed _v2", true));
-		events.add(new Event("_v1 stabbed _v2 with a knife",true));
-		events.add(new Event("_v1 skewered _v2 with a spear", true));
-		events.add(new Event("_v1 fell off a cliff", false));
-		events.add(new Event("_v1 shot _v2", true));
-		events.add(new Event("_v1 died after eating poisoned food", false));
-		events.add(new Event("_v1 was stung to death by a swarm of hornets", false));
-		events.add(new Event("_v1 crushed _v2's skull with a rock", true));
-		events.add(new Event("_v1 decapitated _v2 with a machete", true));
-		events.add(new Event("_v1 was flattened under a landslide", false));
-		ArrayList<Event> nonLethal = new ArrayList<Event>();
-		nonLethal.add(new Event("_v1 bitch-slapped _v2", true));
-		nonLethal.add(new Event("_v1 slipped while climbing a tree and broke their leg", false));
-		nonLethal.add(new Event("_v1 and _v2 decided to work together", true));
-		nonLethal.add(new Event("_v1 was injured trying to escape from _v2", true));
-		nonLethal.add(new Event("_v1 got sick after drinking contaminated water", false));
-		nonLethal.add(new Event("_v1 and _v2 got into a fistfight", true));
-		nonLethal.add(new Event("_v1 made a spear", false, spear));
-		nonLethal.add(new Event("_v1 found a sword", false, sword));
-		nonLethal.add(new Event("_v1 found a pocket knife", false, pocketKnife));
-		nonLethal.add(new Event("_v1 found a switchblade", false, switchblade));
-		nonLethal.add(new Event("_v1 found a bowie knife", false, bowieKnife));
-		nonLethal.add(new Event("_v1 found a pistol", false, pistol));
-		nonLethal.add(new Event("_v1 found a rifle", false, rifle));
-		nonLethal.add(new Event("_v1 found a wrench", false, wrench));
-		nonLethal.add(new Event("_v1 found a pipe", false, pipe));
-		nonLethal.add(new Event("_v1 made a club", false, club));
-		nonLethal.add(new Event("_v1 found a bow", false, bow));
-		nonLethal.add(new Event("_v1 found a crossbow", false, crossbow));
-		nonLethal.add(new Event("_v1 found a hatchet", false, hatchet));
-		nonLethal.add(new Event("_v1 found an axe", false, axe));
+		ArrayList<Event> events = lethalEvents();
+		
+		ArrayList<Event> nonLethal = nonlethalEvents();
+		
 		data = new GameData(new ArrayList<Tribute>(), events, new ArrayList<Event>(), new  ArrayList<Item>(), 5, nonLethal, 2);
 
 	}
@@ -246,14 +203,14 @@ public class HungerGames extends JFrame implements ActionListener{
 				cardPanel.add(setUpPanel, "Setup");
 				data.resetDay();
 				data.setUsers((ArrayList<Tribute>)tributes.clone());
-
+				
 				while(data.getUsers().size() != 1) {
 					ArrayList<String> events = data.nextDay();
-
+					
 					JPanel card = new JPanel();
 					card.setLayout(new GridBagLayout());
 					GridBagConstraints c = new GridBagConstraints();
-
+					
 					//Current Day
 					c.anchor = GridBagConstraints.NORTH;
 					c.gridx = 0;
@@ -275,10 +232,10 @@ public class HungerGames extends JFrame implements ActionListener{
 					c.ipady = 10;
 					c.gridwidth = 4;
 					card.add(eventPanel, c);
-
+				
 					String females = getFemaleList(tributes);
 					String males = getMaleList(tributes);
-
+					
 					//list of male tributes
 					c.gridx = 1;
 					c.gridy = 2;
@@ -287,7 +244,7 @@ public class HungerGames extends JFrame implements ActionListener{
 					c.gridwidth = 1;
 					c.fill = GridBagConstraints.NONE;
 					card.add(new JLabel(males), c);
-
+					
 					//list of female tributes
 					c.anchor = GridBagConstraints.NORTHEAST;
 					c.gridx = 2;
@@ -296,9 +253,9 @@ public class HungerGames extends JFrame implements ActionListener{
 					c.ipady = 20;
 					c.gridwidth = 1;
 					card.add(new JLabel(females, SwingConstants.RIGHT), c);
+					
 
-
-
+					
 					if(data.getUsers().size() != 1) {
 						nextDay = new JButton("Next Day");
 					}
@@ -308,28 +265,28 @@ public class HungerGames extends JFrame implements ActionListener{
 					nextDay.addActionListener(this);
 					prevDay = new JButton("Previous Day");
 					prevDay.addActionListener(this);
-
-
+					
+					
 					c.gridx = 3;
 					c.gridy = 3;
 					c.ipadx = 20;
 					c.ipady = 20;
 					c.gridwidth = 1;
 					card.add(nextDay, c);
-
-
+					
+					
 					c.gridx = 0;
 					c.gridy = 3;
 					c.ipadx = 20;
 					c.ipady = 20;
 					c.gridwidth = 1;
 					card.add(prevDay, c);
-
+					
 					//add panel to cards
 					cardPanel.add(card, "Day " + data.getDay());
-
+					
 				}
-				//				JOptionPane.showMessageDialog(null, data.getUsers().get(0).getName() + " is the winner!");
+//				JOptionPane.showMessageDialog(null, data.getUsers().get(0).getName() + " is the winner!");
 				CardLayout l = (CardLayout) cardPanel.getLayout();
 				l.next(cardPanel);
 			}
@@ -337,7 +294,7 @@ public class HungerGames extends JFrame implements ActionListener{
 		else if(e.getSource() == autofill){
 			ArrayList<String> males = data.getMaleNames();
 			ArrayList<String> females = data.getFemaleNames();
-
+			
 			for(int i = 0; i < 12; i++) {
 				maleTributes[i].setText(males.get(i));
 				femaleTributes[i].setText(females.get(i));
@@ -368,7 +325,7 @@ public class HungerGames extends JFrame implements ActionListener{
 		s += "</h4></right></html>";
 		return s;
 	}
-
+	
 	public String getMaleList(ArrayList<Tribute> t){
 		String s = "<html><h4>Males<br><br>";
 		for(int i = 0; i < t.size(); i++)
@@ -401,7 +358,7 @@ public class HungerGames extends JFrame implements ActionListener{
 	}
 
 
-
+	
 	public static ArrayList<Event> lethalEvents() {
 		ArrayList<Event> events = new ArrayList<Event>();
 		events.add(new Event("_v1 killed _v2 with a rock", true));
@@ -437,6 +394,16 @@ public class HungerGames extends JFrame implements ActionListener{
 		events.add(new Event("_v1 bled to death", false));
 		events.add(new Event("_v1 fell on their own sword", false));
 		events.add(new Event("_v1 was eaten by _v2", true));
+		events.add(new Event("_v1 killed _v2", true));
+		events.add(new Event("_v1 stabbed _v2 with a knife",true));
+		events.add(new Event("_v1 skewered _v2 with a spear", true));
+		events.add(new Event("_v1 fell off a cliff", false));
+		events.add(new Event("_v1 shot _v2", true));
+		events.add(new Event("_v1 died after eating poisoned food", false));
+		events.add(new Event("_v1 was stung to death by a swarm of hornets", false));
+		events.add(new Event("_v1 crushed _v2's skull with a rock", true));
+		events.add(new Event("_v1 decapitated _v2 with a machete", true));
+		events.add(new Event("_v1 was flattened under a landslide", false));
 		return events;
 	}
 
@@ -470,6 +437,13 @@ public class HungerGames extends JFrame implements ActionListener{
 		events.add(new Event("_v1 caught a cold", false));
 		events.add(new Event("_v1 saved _v2’s life", true));
 		events.add(new Event("_v1 and _v2 make an alliance", true));
+		//events.add(new Event("_v1 bitch-slapped _v2", true));
+		events.add(new Event("_v1 slipped while climbing a tree and broke their leg", false));
+		events.add(new Event("_v1 and _v2 decided to work together", true));
+		events.add(new Event("_v1 was injured trying to escape from _v2", true));
+		events.add(new Event("_v1 got sick after drinking contaminated water", false));
+		events.add(new Event("_v1 and _v2 got into a fistfight", true));
+		events.add(new Event("_v1 built a wall", true));
 		return events;
 	}
 }
