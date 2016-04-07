08a60787b50ba8e44fa0a22241d9c276d9efde53
mlfraser <monicafraser9@gmail.com>
2016-04-07 16:54:20 -0400

    Added user settings

--- src/HungerGames.java
+++ src/HungerGames.java
@@ -6,6 +6,7 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ import javax.swing.border.EmptyBorder;
 
 public class HungerGames extends JFrame implements ActionListener{
 	private JButton submit;
-	static GameData data;
+	public static GameData data;
 	JLabel[] districts;
 	JTextField[] maleTributes;
 	JTextField[] femaleTributes;
@@ -26,6 +27,8 @@ public class HungerGames extends JFrame implements ActionListener{
 	JButton autofill;
 	JButton userSettings;
 	JPanel setUpPanel;
+	static UserSettings settings;
+	static ArrayList<Item> items;
 
 	public HungerGames(String title){
 		super(title);
@@ -156,6 +159,7 @@ public class HungerGames extends JFrame implements ActionListener{
 		cardPanel.add(setUpPanel, "Setup");
 		CardLayout l = (CardLayout) cardPanel.getLayout();
 		l.show(cardPanel, "Setup");
+		
 	}
 
 	public static void main(String[] args) {
@@ -166,54 +170,16 @@ public class HungerGames extends JFrame implements ActionListener{
 		app.pack();
 		app.setVisible(true);
 
-		Item spear = new Item("spear", 20);
-		Item sword = new Item("sword", 20);
-		Item pocketKnife = new Item("pocket knife", 5);
-		Item switchblade = new Item("switchblade", 5);
-		Item bowieKnife = new Item("bowie knife", 10);
-		Item pistol = new Item("pistol", 25);
-		Item rifle = new Item("rifle", 30);
-		Item wrench = new Item("wrench", 5);
-		Item pipe = new Item("pipe", 5);
-		Item club = new Item("club", 5);
-		Item bow = new Item("bow", 20);
-		Item crossbow = new Item("crossbow", 25);
-		Item hatchet = new Item("hatchet", 10);
-		Item axe = new Item("axe", 20);
-
-		ArrayList<Event> events = new ArrayList<Event>();
-		events.add(new Event("_v1 killed _v2", true));
-		events.add(new Event("_v1 stabbed _v2 with a knife",true));
-		events.add(new Event("_v1 skewered _v2 with a spear", true));
-		events.add(new Event("_v1 fell off a cliff", false));
-		events.add(new Event("_v1 shot _v2", true));
-		events.add(new Event("_v1 died after eating poisoned food", false));
-		events.add(new Event("_v1 was stung to death by a swarm of hornets", false));
-		events.add(new Event("_v1 crushed _v2's skull with a rock", true));
-		events.add(new Event("_v1 decapitated _v2 with a machete", true));
-		events.add(new Event("_v1 was flattened under a landslide", false));
-		ArrayList<Event> nonLethal = new ArrayList<Event>();
-		nonLethal.add(new Event("_v1 bitch-slapped _v2", true));
-		nonLethal.add(new Event("_v1 slipped while climbing a tree and broke their leg", false));
-		nonLethal.add(new Event("_v1 and _v2 decided to work together", true));
-		nonLethal.add(new Event("_v1 was injured trying to escape from _v2", true));
-		nonLethal.add(new Event("_v1 got sick after drinking contaminated water", false));
-		nonLethal.add(new Event("_v1 and _v2 got into a fistfight", true));
-		nonLethal.add(new Event("_v1 made a spear", false, spear));
-		nonLethal.add(new Event("_v1 found a sword", false, sword));
-		nonLethal.add(new Event("_v1 found a pocket knife", false, pocketKnife));
-		nonLethal.add(new Event("_v1 found a switchblade", false, switchblade));
-		nonLethal.add(new Event("_v1 found a bowie knife", false, bowieKnife));
-		nonLethal.add(new Event("_v1 found a pistol", false, pistol));
-		nonLethal.add(new Event("_v1 found a rifle", false, rifle));
-		nonLethal.add(new Event("_v1 found a wrench", false, wrench));
-		nonLethal.add(new Event("_v1 found a pipe", false, pipe));
-		nonLethal.add(new Event("_v1 made a club", false, club));
-		nonLethal.add(new Event("_v1 found a bow", false, bow));
-		nonLethal.add(new Event("_v1 found a crossbow", false, crossbow));
-		nonLethal.add(new Event("_v1 found a hatchet", false, hatchet));
-		nonLethal.add(new Event("_v1 found an axe", false, axe));
-		data = new GameData(new ArrayList<Tribute>(), events, new ArrayList<Event>(), new  ArrayList<Item>(), 5, nonLethal, 2);
+		settings = null;
+		settings = new UserSettings(settings, "User Settings");
+		settings.setSize(500, 300);
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (int) ((dimension.getWidth() - settings.getWidth()) / 2);
+		int y = (int) ((dimension.getHeight() - settings.getHeight()) / 2);
+		settings.setLocation(x, y);
+		settings.init();
+		items = new ArrayList<Item>();
+		data = new GameData(new ArrayList<Tribute>(), lethalEvents(), new ArrayList<Event>(), 5, nonlethalEvents(), items, 2);
 
 	}
 	@Override
@@ -242,6 +208,9 @@ public class HungerGames extends JFrame implements ActionListener{
 				JOptionPane.showMessageDialog(null, "You need to enter 24 names before starting.");
 			}
 			else {
+				if(!data.getResurect().equals("None")) {
+					data.setHasResurected(false);
+				}
 				cardPanel.removeAll();
 				cardPanel.add(setUpPanel, "Setup");
 				data.resetDay();
@@ -259,7 +228,7 @@ public class HungerGames extends JFrame implements ActionListener{
 					c.gridx = 0;
 					c.gridy = 0;
 					c.ipadx = 20;
-					c.ipady = 20;
+					c.ipady = 10;
 					c.gridwidth = 4;
 					c.weighty = 1;
 					card.add(new JLabel("<html><h1>Day " + Integer.toString(data.getDay()) + "</h1></html>"), c);
@@ -272,7 +241,7 @@ public class HungerGames extends JFrame implements ActionListener{
 					c.gridx = 0;
 					c.gridy = 1;
 					c.ipadx = 20;
-					c.ipady = 10;
+					c.ipady = 5;
 					c.gridwidth = 4;
 					card.add(eventPanel, c);
 
@@ -351,6 +320,10 @@ public class HungerGames extends JFrame implements ActionListener{
 			CardLayout l = (CardLayout) cardPanel.getLayout();
 			l.previous(cardPanel);
 		}
+		else if(e.getSource() == userSettings) {
+			
+			settings.setVisible(true);
+		}
 	}
 	public String getFemaleList(ArrayList<Tribute> t) {
 		String s = "<html><right><h4>Females<br><br>";
@@ -404,72 +377,153 @@ public class HungerGames extends JFrame implements ActionListener{
 
 	public static ArrayList<Event> lethalEvents() {
 		ArrayList<Event> events = new ArrayList<Event>();
-		events.add(new Event("_v1 killed _v2 with a rock", true));
-		events.add(new Event("_v1 shot _v2 with a gun", true));
-		events.add(new Event("_v1 stabbed _v2 with a knife", true));
-		events.add(new Event("_v1 strangled _v2", true));
-		events.add(new Event("_v1 beheaded _v2 with an axe", true));
-		events.add(new Event("_v1 broke _v2’s neck", true));
-		events.add(new Event("_v1 fell out of a tree and died", false));
-		events.add(new Event("_v1 died from dehydration", false));
-		events.add(new Event("_v1 fell off a cliff", false));
-		events.add(new Event("_v1 pushed _v2 off a cliff", true));
-		events.add(new Event("_v1 ran into the forcefield", false));
-		events.add(new Event("_v1 killed _v2 with a bow and arrow", true));
-		events.add(new Event("_v1 was killed by tracker jacker stings", false));
-		events.add(new Event("_v1 was killed by mutts", false));
-		events.add(new Event("_v1 beat _v2 to death", true));
-		events.add(new Event("_v1 died from starvation", false));
-		events.add(new Event("_v1 died from hypothermia", false));
-		events.add(new Event("_v1 drowned in the lake", false));
-		events.add(new Event("_v1 died from an infected wound", false));
-		events.add(new Event("_v1 pushed _v2 into the forcefield", true));
-		events.add(new Event("_v1 was blown away by landmines", false));
-		events.add(new Event("_v1 lit _v2 on fire", true));
-		events.add(new Event("_v1 fell into a pit and died", false));
-		events.add(new Event("_v1 stabbed _v2 with a tree branch", true));
-		events.add(new Event("_v1 killed _v2 in their sleep", true));
-		events.add(new Event("_v1 was killed by _v2 while trying to cook food", true));
-		events.add(new Event("_v1 was killed by poisonous gas", false));
-		events.add(new Event("_v1 was struck by lightning", false));
-		events.add(new Event("_v1 was killed by a landslide", false));
-		events.add(new Event("_v1 ate poisonous berries", false));
-		events.add(new Event("_v1 bled to death", false));
-		events.add(new Event("_v1 fell on their own sword", false));
-		events.add(new Event("_v1 was eaten by _v2", true));
+		
+		
+		if(data == null ||data.getGore().equals("Medium")) {
+			events.add(new Event("_v1 killed _v2 with a rock", true));
+			events.add(new Event("_v1 shot _v2 with a gun", true));
+			events.add(new Event("_v1 stabbed _v2 with a knife", true));
+			events.add(new Event("_v1 strangled _v2", true));
+			events.add(new Event("_v1 beheaded _v2 with an axe", true));
+			events.add(new Event("_v1 broke _v2’s neck", true));
+			events.add(new Event("_v1 fell out of a tree and died", false));
+			events.add(new Event("_v1 died from dehydration", false));
+			events.add(new Event("_v1 fell off a cliff", false));
+			events.add(new Event("_v1 pushed _v2 off a cliff", true));
+			events.add(new Event("_v1 ran into the forcefield", false));
+			events.add(new Event("_v1 killed _v2 with a bow and arrow", true));
+			events.add(new Event("_v1 was killed by tracker jacker stings", false));
+			events.add(new Event("_v1 was killed by mutts", false));
+			events.add(new Event("_v1 beat _v2 to death", true));
+			events.add(new Event("_v1 died from starvation", false));
+			events.add(new Event("_v1 died from hypothermia", false));
+			events.add(new Event("_v1 drowned in the lake", false));
+			events.add(new Event("_v1 died from an infected wound", false));
+			events.add(new Event("_v1 pushed _v2 into the forcefield", true));
+			events.add(new Event("_v1 was blown away by landmines", false));
+			events.add(new Event("_v1 lit _v2 on fire", true));
+			events.add(new Event("_v1 fell into a pit and died", false));
+			events.add(new Event("_v1 stabbed _v2 with a tree branch", true));
+			events.add(new Event("_v1 killed _v2 in their sleep", true));
+			events.add(new Event("_v2 was killed by _v1 while trying to cook food", true));
+			events.add(new Event("_v1 was killed by poisonous gas", false));
+			events.add(new Event("_v1 was struck by lightning", false));
+			events.add(new Event("_v1 was killed by a landslide", false));
+			events.add(new Event("_v1 ate poisonous berries", false));
+			events.add(new Event("_v1 bled to death", false));
+			events.add(new Event("_v1 fell on their own sword", false));
+			events.add(new Event("_v1 was eaten by _v2", true));
+			events.add(new Event("_v1 killed _v2", true));
+			events.add(new Event("_v1 stabbed _v2 with a knife",true));
+			events.add(new Event("_v1 skewered _v2 with a spear", true));
+			events.add(new Event("_v1 fell off a cliff", false));
+			events.add(new Event("_v1 shot _v2", true));
+			events.add(new Event("_v1 died after eating poisoned food", false));
+			events.add(new Event("_v1 was stung to death by a swarm of hornets", false));
+			events.add(new Event("_v1 crushed _v2's skull with a rock", true));
+			events.add(new Event("_v1 decapitated _v2 with a machete", true));
+			events.add(new Event("_v1 was flattened under a landslide", false));
+		}
+		else if(data.getGore().equals("Low")) {
+			events.add(new Event("_v1 didn't eat their vegetables", false));
+			events.add(new Event("_v1 got into an unmarked white van", false));
+		}
+		else if(data.getGore().equals("High")){
+			events.add(new Event("_v1 sawed off his own led for food", false));
+		}
 		return events;
 	}
 
 	public static ArrayList<Event> nonlethalEvents() {
+		Item spear = new Item("spear", 20);
+		Item sword = new Item("sword", 20);
+		Item pocketKnife = new Item("pocket knife", 5);
+		Item switchblade = new Item("switchblade", 5);
+		Item bowieKnife = new Item("bowie knife", 10);
+		Item pistol = new Item("pistol", 25);
+		Item rifle = new Item("rifle", 30);
+		Item wrench = new Item("wrench", 5);
+		Item pipe = new Item("pipe", 5);
+		Item club = new Item("club", 5);
+		Item bow = new Item("bow", 20);
+		Item crossbow = new Item("crossbow", 25);
+		Item hatchet = new Item("hatchet", 10);
+		Item axe = new Item("axe", 20);
+		items.add(spear);
+		items.add(sword);
+		items.add(pocketKnife);
+		items.add(switchblade);
+		items.add(bowieKnife);
+		items.add(pistol);
+		items.add(rifle);
+		items.add(wrench);
+		items.add(pipe);
+		items.add(club);
+		items.add(bow);
+		items.add(crossbow);
+		items.add(hatchet);
+		items.add(axe);
+		
 		ArrayList<Event> events = new ArrayList<Event>();
-		events.add(new Event("_v1 tripped _v2", true));
-		events.add(new Event("_v1 cut _v2 with a knife", true));
-		events.add(new Event("_v1 stabbed _v2 in the leg", true));
-		events.add(new Event("_v1 fell out of a tree and broke their arm", false));
-		events.add(new Event("_v1 punched _v2 and broke their nose", true));
-		events.add(new Event("_v1 and _v2 got in a fight", true));
-		events.add(new Event("_v1 passed out from exhaustion", false));
-		events.add(new Event("_v1 fell down a hill", false));
-		events.add(new Event("_v1 broke their ankle", false));
-		events.add(new Event("_v1 broke their leg", false));
-		events.add(new Event("_v1 was stung by tracker jackers", false));
-		events.add(new Event("_v1 traveled to higher ground", true));
-		events.add(new Event("_v1 climbed a tree", false));
-		events.add(new Event("_v1 hid in a cave", false));
-		events.add(new Event("_v1 hunted for food", false));
-		events.add(new Event("_v1 found a source of water", false));
-		events.add(new Event("_v1 followed _v2", true));
-		events.add(new Event("_v1 injured themselves", false));
-		events.add(new Event("_v1 attacked _v2, but they escape unharmed", true));
-		events.add(new Event("_v1 built a fire", false));
-		events.add(new Event("_v1 slept through the night", false));
-		events.add(new Event("_v1 received a gift from home", false));
-		events.add(new Event("_v1 and _v2 made a truce for the night", true));
-		events.add(new Event("_v1 chased _v2 deep into the woods", true));
-		events.add(new Event("_v1 sprained their ankle while running away", false));
-		events.add(new Event("_v1 caught a cold", false));
-		events.add(new Event("_v1 saved _v2’s life", true));
-		events.add(new Event("_v1 and _v2 make an alliance", true));
+		
+		if(data == null || data.getGore().equals("Medium")) {
+			events.add(new Event("_v1 tripped _v2", true));
+			events.add(new Event("_v1 cut _v2 with a knife", true));
+			events.add(new Event("_v1 stabbed _v2 in the leg", true));
+			events.add(new Event("_v1 fell out of a tree and broke their arm", false));
+			events.add(new Event("_v1 punched _v2 and broke their nose", true));
+			events.add(new Event("_v1 and _v2 got in a fight", true));
+			events.add(new Event("_v1 passed out from exhaustion", false));
+			events.add(new Event("_v1 fell down a hill", false));
+			events.add(new Event("_v1 broke their ankle", false));
+			events.add(new Event("_v1 broke their leg", false));
+			events.add(new Event("_v1 was stung by tracker jackers", false));
+			events.add(new Event("_v1 traveled to higher ground", true));
+			events.add(new Event("_v1 climbed a tree", false));
+			events.add(new Event("_v1 hid in a cave", false));
+			events.add(new Event("_v1 hunted for food", false));
+			events.add(new Event("_v1 found a source of water", false));
+			events.add(new Event("_v1 followed _v2", true));
+			events.add(new Event("_v1 injured themselves", false));
+			events.add(new Event("_v1 attacked _v2, but they escape unharmed", true));
+			events.add(new Event("_v1 built a fire", false));
+			events.add(new Event("_v1 slept through the night", false));
+			events.add(new Event("_v1 received a gift from home", false));
+			events.add(new Event("_v1 and _v2 made a truce for the night", true));
+			events.add(new Event("_v1 chased _v2 deep into the woods", true));
+			events.add(new Event("_v1 sprained their ankle while running away", false));
+			events.add(new Event("_v1 caught a cold", false));
+			events.add(new Event("_v1 saved _v2’s life", true));
+			events.add(new Event("_v1 and _v2 make an alliance", true));
+			events.add(new Event("_v1 bitch-slapped _v2", true));
+			events.add(new Event("_v1 slipped while climbing a tree and broke their leg", false));
+			events.add(new Event("_v1 and _v2 decided to work together", true));
+			events.add(new Event("_v1 was injured trying to escape from _v2", true));
+			events.add(new Event("_v1 got sick after drinking contaminated water", false));
+			events.add(new Event("_v1 and _v2 got into a fistfight", true));
+			events.add(new Event("_v1 made a spear", false, spear));
+			events.add(new Event("_v1 found a sword", false, sword));
+			events.add(new Event("_v1 found a pocket knife", false, pocketKnife));
+			events.add(new Event("_v1 found a switchblade", false, switchblade));
+			events.add(new Event("_v1 found a bowie knife", false, bowieKnife));
+			events.add(new Event("_v1 found a pistol", false, pistol));
+			events.add(new Event("_v1 found a rifle", false, rifle));
+			events.add(new Event("_v1 found a wrench", false, wrench));
+			events.add(new Event("_v1 found a pipe", false, pipe));
+			events.add(new Event("_v1 made a club", false, club));
+			events.add(new Event("_v1 found a bow", false, bow));
+			events.add(new Event("_v1 found a crossbow", false, crossbow));
+			events.add(new Event("_v1 found a hatchet", false, hatchet));
+			events.add(new Event("_v1 found an axe", false, axe));
+		}
+		else if(data.getGore().equals("Low")) {
+			events.add(new Event("_v1 got put in timeout", false));
+			events.add(new Event("_v1 took a nap", false));
+		}
+		else if(data.getGore().equals("High")) {
+			events.add(new Event("_v1 gouged out the eyes of _v2", true));
+			
+		}
 		return events;
 	}
 }
